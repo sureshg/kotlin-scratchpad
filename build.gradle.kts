8099065a1fe94f824dee3446cab44218f8a2cb7f
Suresh G <sureshg.g@gmail.com>
2018-03-18 22:17:39 -0700

    Build script changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,24 @@
-import org.jetbrains.kotlin.gradle.dsl.Coroutines.ENABLE
+import org.jetbrains.kotlin.gradle.dsl.Coroutines
+import org.jetbrains.kotlin.gradle.tasks.*
 
 plugins {
     application
     kotlin("jvm") version "1.2.30"
 }
 
+group = "io.sureshg"
+version = "1.0-SNAPSHOT"
+
 application {
     mainClassName = "io.sureshg.MainKt"
 }
 
 kotlin {
-    experimental.coroutines = ENABLE
+    experimental.coroutines = Coroutines.ENABLE
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
 }
 
 repositories {
@@ -18,6 +26,6 @@ repositories {
 }
 
 dependencies {
-    compile(kotlin("stdlib"))
+    compile(kotlin("stdlib-jdk8"))
+    compile("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "0.22.5")
 }
-
